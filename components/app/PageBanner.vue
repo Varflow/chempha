739441fa5b13ff69d0d0ccbbfd7dd546c05ff675
DimaDevelopment <dima.development@fakel.com.ua>
2023-07-24 23:31:56 +0300

    fixed page-banner

--- components/app/PageBanner.vue
+++ components/app/PageBanner.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="page-banner">
+  <div class="page-banner" :class="{ 'with-image': Boolean(img) }">
     <img :src="img" alt="" v-if="Boolean(img)" class="page-banner__img" />
-    <h3 class="page-banner__title" :class="{ 'with-image': Boolean(img) }">{{ title }}</h3>
+    <h3 class="page-banner__title">{{ title }}</h3>
     <h4 class="page-banner__subtitle" v-if="Boolean(subtitle)">{{ subtitle }}</h4>
   </div>
 </template>
