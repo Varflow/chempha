e771de7ce131306c626f85e5528c5b001bb184d8
DimaDevelopment <dima.development@fakel.com.ua>
2023-08-08 22:42:49 +0300

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,33 @@ export default defineNuxtConfig({
           href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
           crossorigin: "",
         },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/favicon/site.webmanifest",
+        },
+        {
+          rel: "mask-icon",
+          type: "image/png",
+          href: "/favicon/safari-pinned-tab.svg",
+          color: "#5bbad5",
+        },
       ],
     },
   },
