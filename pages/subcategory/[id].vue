14c27c44372fe5d1f872c5c782ba62b630996d32
DimaDevelopment <dima.development@fakel.com.ua>
2023-08-08 22:49:55 +0300

    fixed product url

--- pages/subcategory/[id].vue
+++ pages/subcategory/[id].vue
@@ -14,7 +14,7 @@
             :id="product.id"
             :name="product.name"
             :category="product.category"
-            :image="`${media}${product.image.url}`"
+            :image="`${media}${product.image?.url}`"
           />
         </div>
       </div>
