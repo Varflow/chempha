278001ca1e1bbe774f7d4153a05a233f3e542f97
DimaDevelopment <dima.development@fakel.com.ua>
2023-11-06 11:26:10 +0200

    add canonical link rel

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="home-page">
     <Head>
       <Title>Chempha</Title>
+      <Link rel="canonical" href="https://chempha.com" />
       <Meta
         name="google-site-verification"
         content="t5h28M5twka3Ii02_z1gXTKtr-UN0KOGhHbq619dCbQ"
