2e1ebec9aea4cbf23eedc4f75d7ad19a8764d88a
dev_wandry <dima.development@wandry.com.ua>
2026-05-01 19:10:57 +0300

    fix: clear page when local change

--- app/components/app/AppLocale.vue
+++ app/components/app/AppLocale.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="header-locales">
-    <NuxtLink :to="$switchLocalePath('en')" style="text-decoration: none">
+    <NuxtLink :to="switchTo('en')" :replace style="text-decoration: none">
       <img src="/images/flags/en.svg" alt="English" />
-      <!-- <span class="header-menu__link"> EN </span> -->
     </NuxtLink>
-    <NuxtLink :to="$switchLocalePath('ee')" style="text-decoration: none">
+    <NuxtLink :to="switchTo('ee')" :replace style="text-decoration: none">
       <img src="/images/flags/ee.svg" alt="Eesti" />
-      <!-- <span class="header-menu__link"> EE </span> -->
     </NuxtLink>
-    <NuxtLink :to="$switchLocalePath('lv')" style="text-decoration: none">
+    <NuxtLink :to="switchTo('lv')" :replace style="text-decoration: none">
       <img src="/images/flags/lv.svg" alt="Latvian" />
-      <!-- <span class="header-menu__link"> LV </span> -->
     </NuxtLink>
-    <NuxtLink :to="$switchLocalePath('lt')" style="text-decoration: none">
+    <NuxtLink :to="switchTo('lt')" :replace style="text-decoration: none">
       <img src="/images/flags/lt.svg" alt="Lithuanian" />
-      <!-- <span class="header-menu__link"> LT </span> -->
     </NuxtLink>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+const switchLocalePath = useSwitchLocalePath();
+
+const switchTo = (loc) => ({
+  path: switchLocalePath(loc).split("?")[0] || "/",
+  query: {},
+});
 </script>
